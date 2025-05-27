10830aa1a7503238081e456dec4a2c5077e411cf
Olli Sund <olli.sund@tuni.fi>
2025-05-27 18:02:05 +0300

    Initialize project.

---
+++ plant-doctor/app/_layout.jsx
@@ -0,0 +1,8 @@
+import { Stack } from "expo-router";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+const RootLayout = () => {
+  return <Stack />;
+}
+
+export default RootLayout;
